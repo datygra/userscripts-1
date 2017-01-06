8c8abb15d0b46b0d1d9a3bb4124ea3390e5d1ea2
smac89 <smac89@users.noreply.github.com>
2017-01-06 13:12:51 -0600

    Updated the script, and override some css

--- codehiliteme/userscript.js
+++ codehiliteme/userscript.js
@@ -6,87 +6,76 @@
 // @author       smac89
 // @include      */*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/highlight.min.js
+// @require      https://raw.githubusercontent.com/smac89/userscripts/master/codehiliteme/highlight.js/build/highlight.min.js
 // @run-at       document-end
-// @resource     hljs.css    //cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/default.min.css
+// @resource     hljs.css    https://raw.githubusercontent.com/smac89/userscripts/master/codehiliteme/highlight.js/build/styles/default.min.css
+// @resource     langs.json  https://raw.githubusercontent.com/smac89/userscripts/master/codehiliteme/supported_langs.json
 // @grant        GM_xmlhttpRequest
 // @grant        GM_getResourceText
+// @grant        GM_addStyle
 // ==/UserScript==
 
 /* NOTES
 * https://greasyfork.org/en/help/meta-keys
 * https://highlightjs.org/static/demo/
-// @match https://raw.githubusercontent.com/*
-*
 */
 
-// Simply enter the name of the extension you want to detect
-// and assign it a highlighter, then let us do the rest
 
+// Give the option of customizing the output to use the browser's built-in pretty printer
 var STYLE_OPTIONS =  {
     style: "None",
     linenos: false
 };
 
-let aliases;
 
-// Give the option of customizing the output to use the browser's built-in pretty printer
+var CodeHiliteMe = (function() {
+    const _private = this;
+    const extension_map = $.parseJSON(GM_getResourceText("langs.json"));
+
+    // Add the highlight js stylesheet
+    GM_addStyle(GM_getResourceText('hljs.css'));
+
+    return {
+        hiliteit: function() {
+            $('pre code').each(function(i, block) {
+                hljs.highlightBlock(block);
+            });
+        },
+
+        ishilitable: function() {
+            var $code = $("body > pre:first-child");
+            var filename = window.location.href.split("/").pop().toLowerCase();
+
+            if ($code.exists() && filename) {
+
+                var ext = filename.substr(filename.lastIndexOf('.')).toLowerCase();
+                var language = '';
+
+                // if the extension is not found, then attempt to treat the file as a special file
+                if (ext in extension_map) {
+                    language = extension_map[ext];
+                } else if (filename in extension_map) {
+                    language = extension_map[filename];
+                } else {
+                    return false;
+                }
+
+                var text = $code.text();
+                $code.replaceWith(`<pre><code class="${language}"></code></pre>`);
+                $('code').text(text);
+
+                return true;
+            }
+            return false;
+        }
+    };
+}());
 
 $(function() {
     'use strict';
 
-    var $code = $("body > pre:first");
-    var file = window.location.href.split("/").pop();
-    
-    alert(hljs.listLanguages());
-
-    if ($code.exists()) {
-
-        
-
-
-        // GM_xmlhttpRequest({
-        //     url: window.location.href,
-        //     method: "HEAD",
-        //     // headers: {
-        //     //     "Accept-Charset": "utf-8",
-        //     //     "Accept": "text/html",
-        //     //     "Content-Type": "application/x-www-form-urlencoded"
-        //     // },
-        //     onload: function(resp) {
-        //         console.log(resp.responseHeaders);
-        //         alert(resp.responseHeaders.match(/^content-type:\W+(.+)/gim));
-        //         // $code.replaceWith(resp.responseText);
-        //     },
-        //     onerror: function(resp) {
-        //         alert(resp.responseText);
-        //     }
-        // });
-
-        // GM_xmlhttpRequest({
-        //     url: "http://hilite.me/api",
-        //     method: "POST",
-        //     data: {
-        //         code: escape($code.text()),
-        //         lexer: "python",
-        //         style: STYLES[STYLE_OPTIONS[file_ext] || STYLE_OPTIONS['default']],
-        //         // style: 'fruity',
-        //         linenos: true,
-        //         options: ['stripnl'] //http://pygments.org/docs/lexers/
-        //     },
-        //     headers: {
-        //         "Accept-Charset": "utf-8",
-        //         "Accept": "text/html",
-        //         "Content-Type": "application/x-www-form-urlencoded"
-        //     },
-        //     onload: function(resp) {
-        //         alert(resp.statusText);
-        //         $code.replaceWith(resp.responseText);
-        //     },
-        //     onerror: function(resp) {
-        //         alert("Request failed!" + resp.responseText);
-        //     }
-        // });
+    if (CodeHiliteMe.ishilitable()) {
+        CodeHiliteMe.hiliteit();
     }
 });
 
@@ -96,3 +85,59 @@ $.fn.extend({
     }
 });
 
+
+// GM_xmlhttpRequest({
+//     url: window.location.href,
+//     method: "HEAD",
+//     // headers: {
+//     //     "Accept-Charset": "utf-8",
+//     //     "Accept": "text/html",
+//     //     "Content-Type": "application/x-www-form-urlencoded"
+//     // },
+//     onload: function(resp) {
+//         console.log(resp.responseHeaders);
+//         alert(resp.responseHeaders.match(/^content-type:\W+(.+)/gim));
+//         // $code.replaceWith(resp.responseText);
+//     },
+//     onerror: function(resp) {
+//         alert(resp.responseText);
+//     }
+// });
+
+// GM_xmlhttpRequest({
+//     url: "http://hilite.me/api",
+//     method: "POST",
+//     data: {
+//         code: escape($code.text()),
+//         lexer: "python",
+//         style: STYLES[STYLE_OPTIONS[file_ext] || STYLE_OPTIONS['default']],
+//         // style: 'fruity',
+//         linenos: true,
+//         options: ['stripnl'] //http://pygments.org/docs/lexers/
+//     },
+//     headers: {
+//         "Accept-Charset": "utf-8",
+//         "Accept": "text/html",
+//         "Content-Type": "application/x-www-form-urlencoded"
+//     },
+//     onload: function(resp) {
+//         alert(resp.statusText);
+//         $code.replaceWith(resp.responseText);
+//     },
+//     onerror: function(resp) {
+//         alert("Request failed!" + resp.responseText);
+//     }
+// });
+
+// var blob = new Blob([$.parseHTML(`<pre><div class="${language}">${$code.text()}</div></pre>`).join("")], {
+//     type: 'text/html',
+//     endings: 'native'
+// });
+
+// var reader = new FileReader();
+// reader.onload = function(evt) {
+//     console.log(evt.target.result);
+//     $code.replaceWith(evt.target.result);
+// };
+
+// reader.readAsText(blob);
